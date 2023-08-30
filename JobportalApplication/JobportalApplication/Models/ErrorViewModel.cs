6d62004e7ca1514c3925d487abc9e73c0ee24a35
FarhanTanvirShanto <114474270+FarhanTanvirShanto@users.noreply.github.com>
2023-08-30 11:54:17 +0600

    Project

---
+++ JobportalApplication/JobportalApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JobportalApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
